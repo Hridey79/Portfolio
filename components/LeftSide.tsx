8f5c64d2958e05b1425c3e29b7a75bb9cfc33aff
Hridey <hridey79@gmail.com>
2025-05-12 20:20:48 +0530

    sidebar fixed

--- components/LeftSide.tsx
+++ components/LeftSide.tsx
@@ -1,42 +1,58 @@
-'use client'
+"use client";
 
-import React from 'react'
-import { FiGithub, FiLinkedin, FiInstagram } from "react-icons/fi"
-import {SiCodeforces,SiLeetcode} from "react-icons/si"
-import { motion } from 'framer-motion'
+import React from "react";
+import { FiGithub, FiLinkedin, FiInstagram } from "react-icons/fi";
+import { SiCodeforces, SiLeetcode } from "react-icons/si";
+import { motion } from "framer-motion";
 
-const LeftSide = () => { 
-return (
-    <motion.div className='w-full h-full flex flex-col items-center justify-end gap-4 text-textDark'
-        initial={{x: -100}}
-        animate={{x: 0}}
-        transition={{duration: 0.7}}
-    > 
-    <div className='flex flex-col gap-4'>
-        <a href="https://github.com/Hridey79" target='_blank'>
-            <span className='w-10 h-10 text-xl bg-bodyColor rounded-full inline-flex items-center
+const LeftSide = () => {
+  return (
+    <motion.div
+      className="w-full h-full flex flex-col items-center justify-end gap-4 text-textDark"
+      initial={{ x: -100 }}
+      animate={{ x: 0 }}
+      transition={{ duration: 0.7 }}
+    >
+      <div className="flex flex-col gap-4">
+        <a href="https://github.com/Hridey79" target="_blank">
+          <span
+            className="w-10 h-10 text-xl bg-bodyColor rounded-full inline-flex items-center
             justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 
-            transition-all duration-300'><FiGithub size={28}/></span>
+            transition-all duration-300"
+          >
+            <FiGithub size={28} />
+          </span>
         </a>
-        <a href="https://www.linkedin.com/in/hridey-verma-67633a225/" target='_blank'>
-            <span className='w-10 h-10 text-xl bg-bodyColor rounded-full inline-flex items-center
+        <a
+          href="https://www.linkedin.com/in/hridey-verma-67633a225/"
+          target="_blank"
+        >
+          <span
+            className="w-10 h-10 text-xl bg-bodyColor rounded-full inline-flex items-center
             justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 
-            transition-all duration-300'><FiLinkedin size={28}/></span>
+            transition-all duration-300"
+          >
+            <FiLinkedin size={28} />
+          </span>
         </a>
-        <a href="https://leetcode.com/Hridey79/" target='_blank'>
-            <span className='w-10 h-10 text-xl bg-bodyColor rounded-full inline-flex items-center
+        <a href="https://leetcode.com/Hridey79/" target="_blank">
+          <span
+            className="w-10 h-10 text-xl bg-bodyColor rounded-full inline-flex items-center
             justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 
-            transition-all duration-300'><SiLeetcode size={28}/></span>
+            transition-all duration-300"
+          >
+            <SiLeetcode size={28} />
+          </span>
         </a>
-        <a href="https://github.com/Hridey79" target='_blank'>
+        {/* <a href="https://github.com/Hridey79" target='_blank'>
             <span className='w-10 h-10 text-xl bg-bodyColor rounded-full inline-flex items-center
             justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 
             transition-all duration-300'><SiCodeforces size={28}/></span>
-        </a>
-    </div>
-    <div className='w-[2px] h-60 bg-textDark'></div>
+        </a> */}
+      </div>
+      <div className="w-[2px] h-60 bg-textDark"></div>
     </motion.div>
-)
-}
+  );
+};
 
-export default LeftSide
+export default LeftSide;
