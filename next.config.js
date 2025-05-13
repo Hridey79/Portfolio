8a97142a24a522f3e2ef42deab262be0c728ea74
Hridey <hridey79@gmail.com>
2025-05-13 13:02:11 +0530

    img fix test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    unoptimized: true, // necessary for static hosting (next export)
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
