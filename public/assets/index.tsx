e0b8ffabcdda32a8897f32adf10bbe243dffc48f
Hridey <hridey79@gmail.com>
2025-05-18 10:50:56 +0530

    build fix

--- public/assets/index.tsx
+++ public/assets/index.tsx
@@ -3,11 +3,11 @@ import amazon from "./images/amazonImg.png";
 import pew from "./images/pew.png";
 import blog from "./images/blog.png";
 import churn from "./images/churn.png";
-import interview from "./images/interviewPrep.png";
+import interview from "./images/InterviewPrep.png";
 import pathFinder from "./images/pathFinder.png";
 import LMS from "./images/LMS.png";
 import spotify from "./images/spotify.png";
-import Oscar from "./images/oscar.png";
+import Oscar from "./images/Oscar.png";
 
 export {
   pew,
